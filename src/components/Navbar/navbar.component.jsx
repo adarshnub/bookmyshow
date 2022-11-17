8f41530d01e7a8de487e33c64c0d24a4d9ffafd1
adarshnub <85427511+adarshnub@users.noreply.github.com>
2022-11-17 11:17:27 +0530

    Navbar complete

--- src/components/Navbar/navbar.component.jsx
+++ src/components/Navbar/navbar.component.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {BiChevronRight, BiSearch} from "react-icons/bi";
+import {BiChevronRight, BiSearch, BiMenu, BiChevronDown} from "react-icons/bi";
 
 
 
@@ -18,17 +18,56 @@ const NavSm =()=>{
         </>
     );
 };
-const NavMd =()=>{};
-const NavLg =()=>{};
+const NavMd =()=>{
+    return (
+        <div className="w-full flex items-center gap-3 bg-white px-3 py-2 rounded-md">
+            <BiSearch />
+            <input type="search"
+             className="w-full bg-transparent border-none focus:outline-none" 
+             placeholder="Search for Movies, Events ,Plays ,Sports and Activities" />
+        </div>
+    );
+};
+const NavLg =()=>{
+    return (
+        <>
+            <div className="container mx-auto px-4 flex items-center justify-between">
+                <div className="flex items-center w-3/4 gap-3 ">
+                    <div className="w-20 h-14">
+                    < img src="https://seeklogo.com/images/B/bookmyshow-logo-31BC3C7354-seeklogo.com.png"
+                      alt="logo"
+                      className="w-full h-full"  />
+                    </div> 
+
+                    <div className="w-full flex items-center gap-3 bg-white px-3 py-2 rounded-md">
+                        <BiSearch />
+                        <input type="search"
+                        className="w-full bg-transparent border-none focus:outline-none" 
+                        placeholder="Search for Movies, Events ,Plays ,Sports and Activities" />
+                    </div>
+                </div> 
+                    <div className="flex items-center gap-3">
+                    <span className="text-gray-400 text-xs flex items-center">Kottayam <BiChevronDown /></span>
+                    <button className="bg-red-600 text-white px-2 py-1 text-sm rounded">Sign In</button>
+                    <div className="h-8 w-8 text-white">
+                        <BiMenu className="h-full w-full" />
+                    </div>
+                    </div>           
+            </div>  
+        </>
+    );
+};
+
+
 
 
 const Navbar =()=> {
     return (
         <>
-            <nav className="bg-gray-800 p-4">
+            <nav className="bg-litgray p-4">
                 <div className="md:hidden"> <NavSm /> </div>
-                <div className="hidden md:flex lg:hidden">{/*medium devices*/}</div>
-                <div className="hidden lg:flex">{/*large devices*/}</div>
+                <div className="hidden md:flex lg:hidden"> <NavMd /> </div>
+                <div className="hidden lg:flex"><NavLg /></div>
 
             </nav>
         </>
