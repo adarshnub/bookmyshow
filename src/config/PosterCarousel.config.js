6fa0387d4f94e28496efbf236b02c37c644ec518
adarshnub <85427511+adarshnub@users.noreply.github.com>
2022-11-22 07:35:20 +0530

    bug fix commit

--- src/config/PosterCarousel.config.js
+++ src/config/PosterCarousel.config.js
@@ -1,34 +1,35 @@
 const settings={
     infinity:false,
-    autoplay:false,
+    speed:500,
+   
     slidesToShow:5,
-    slidesToScroll:2,
+    slidesToScroll:4,
     InitialSlide:0,
     responsive: [
         {
-        breakpoints:1024,
+        breakpoint:1024,
         settings : {
             slidesToShow:3,
-            slidesToScroll:2,
+            slidesToScroll:3,
             infinite:true,
 
         },
 
     },
     {
-        breakpoints:600,
+        breakpoint:600,
         settings : {
             slidesToShow:2,
-            slidesToScroll:1,
+            slidesToScroll:2,
             InitialSlide:1,
 
         },
 
     },
     {
-        breakpoints:480,
+        breakpoint:480,
         settings : {
-            slidesToShow:2,
+            slidesToShow:3,
             slidesToScroll:1,
           
 
